056a498dfca4e7ead36a6ad8b79a4df85b0189be
Eduardo Klein <eduardok@impacto.corp.docspider.com.br>
2019-10-08 13:44:14 -0300

    Adding login page.

--- pages/containers/login/style.js
+++ pages/containers/login/style.js
@@ -1,14 +1,27 @@
-const Style = () => (
-    <style jsx="true">
-        {`
-            .container {
-                display: flex;
-                justify-content: center;
-                align-items: center;
-                width: 100%;
-            }
-        `}
-    </style>
-)
+import { makeStyles } from '@material-ui/core/styles'
 
-export default Style;
+const useStyles = makeStyles(theme => ({
+    root: {
+        height: '100vh',
+        overflow: 'hidden',
+    },
+    image: {
+        backgroundImage: 'url(https://source.unsplash.com/random)',
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+    },
+    paper: {
+        margin: theme.spacing(8, 4),
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexDirection: 'column',
+    },
+    avatar: {
+        margin: theme.spacing(1),
+        backgroundColor: theme.palette.secondary.main,
+    },
+}));
+
+export default useStyles;
